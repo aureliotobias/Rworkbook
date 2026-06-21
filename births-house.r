6de8a80e1454d08e2f928d6ceabf558a6d9eafdd
Aurelio Tobias <35725521+aureliotobias@users.noreply.github.com>
2026-06-21 15:25:43 +0200

    Delete births-house.r

--- births-house.r
+++
@@ -1,7 +0,0 @@
-births<-read.table("data/births.csv",head=T,sep=",")
-births <- within(births,
-hyp <- factor(hyp,labels=c("normal","hyper")),
-sex <- factor(sex,labels=c("M","F")),
-agegrp <- cut(matage,breaks=c(20,25,30,35,40,45),right=FALSE),
-gest4 <- cut(gestwks,breaks=c(20,35,37,39,45),right=FALSE))
-
